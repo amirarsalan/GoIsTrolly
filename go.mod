c2d84437a46fdc0f8889a3285b778e09db62490c
Arsalan <am1r.ar3alan@gmail.com>
2025-02-28 04:51:30 +0330

    go is fun

---
+++ go.mod
@@ -0,0 +1,3 @@
+module goproxyctl.kybex.worker.dev/amirarsalan/withoutAuth
+
+go 1.23.4
---
+++ lib.go
@@ -0,0 +1,7 @@
+package goistrolly
+
+import "fmt"
+
+func AreYouTrolled() {
+	fmt.Println("Yes! You're god damn right!")
+}
